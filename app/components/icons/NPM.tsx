b79a25738d7738d76c69ed62907d6a07b4c3d3b0
Kurok1 <khanc.dev@gmail.com>
2025-03-20 07:36:42 +0000

    add changes

--- app/components/icons/NPM.tsx
+++ app/components/icons/NPM.tsx
@@ -1,6 +1,6 @@
 const NPM: React.FC = () => {
     return (
-        <svg t="1742443344202" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="30926" width="25" height="25"><path d="M117.15 906.85V117.16h789.69v789.69zM265.67 265.144v492.533h248.873V367.844H659.57V757.75h98.735V265.144z" fill="#CB3837" p-id="30927"></path></svg>
+        <svg className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="30926" width="25" height="25"><path d="M117.15 906.85V117.16h789.69v789.69zM265.67 265.144v492.533h248.873V367.844H659.57V757.75h98.735V265.144z" fill="#CB3837" p-id="30927"></path></svg>
     )
 }
 
