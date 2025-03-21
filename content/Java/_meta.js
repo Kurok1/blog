2fb4e62f2e2e0e47c215af84fe3d1591f4266971
Kurok1 <khanc.dev@gmail.com>
2025-03-21 17:05:27 +0800

    juc

--- content/Java/_meta.js
+++ content/Java/_meta.js
@@ -1,4 +1,5 @@
 export default {
+    juc : "Java并发编程",
     jvm : "Java虚拟机",
     Spring : "Spring相关"
 }
---
+++ content/Java/juc/_meta.jsx
@@ -0,0 +1,9 @@
+export default {
+    AQS: 'AQS',
+    ReentrantLock: '可重入锁',
+    ReadWriteLock: '读写锁',
+    CountDownLatch: 'CountDownLatch',
+    CyclicBarrier: 'CyclicBarrier',
+    Semaphore: 'Semaphore',
+    ThreadPool: '线程池'
+}
